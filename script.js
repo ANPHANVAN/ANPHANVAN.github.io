dab5ca2855f5e2453fdb94e81a8e747096ad155c
An Phan <2k1.xam@gmail.com>
2025-03-12 15:21:10 +0700

    add some thing

--- script.js
+++ script.js
@@ -23,3 +23,37 @@ function loadPage(page) {
 window.onload = function() {
     loadPage('home');
 };
+
+function showaddress() {
+    document.querySelector("#address button").innerHTML = "An Lạc, Bình Tân, TP.HCM";
+}
+
+// i create a function use querryselectorAll to get all the class skills ul
+function showskills() {
+    let skills = document.querySelectorAll("#skills ul li");
+    // Change the text of the second and third <li> elements
+        // Change the text of the second and third <li> elements
+        skills[1].innerHTML = "Developer skills";
+        skills[2].innerHTML = "Developer skills";
+}
+
+// i create a function replace content project to new content, this content have a 4 tag <p>
+function showproject() {
+    let project = document.querySelector("#projects div");
+    project.innerHTML = `<p>describe 1</p>
+                        <p>describe 2</p>
+                        <p>describe 3</p>
+                        <p>describe 4</p>`;
+}
+
+// This function changes the class of the element with id "changeclass" from "oldClass" to "newClass"
+function changeClass() {
+    let element = document.getElementById("changeclass");
+    element.classList.add("newClass");
+}
+
+
+function toggleBlue() {
+    let element = document.getElementById("changeclass");
+    element.classList.toggle("newClass");
+}
